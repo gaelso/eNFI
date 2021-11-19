ceef79b4cdc44f3a58b227e694a85273178fbd05
gael.sola <gaelsola@hotmail.fr>
2021-11-19 22:44:22 +0700

    completed setup

--- R-setup.R
+++ R-setup.R
@@ -14,10 +14,16 @@ library(usethis)
 library(roxygen2)
 library(learnr)
 
-
 ## Make package
 usethis::create_package("D:/github-repos/eNFI")
 
 ## Make lessons
 usethis::use_tutorial("eNFI-lesson1", "Overview of the preliminary data", open = interactive())
 usethis::use_tutorial("eNFI-lesson2.1", "Simple sampling for carbon - Design", open = interactive())
+
+## Install
+devtools::install()
+
+## Launch lesson
+learnr::run_tutorial("eNFI-lesson1", "eNFI")
+
