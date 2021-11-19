0f9c08e51139d04f68378242244b64f65ae25d7c
gael.sola <gaelsola@hotmail.fr>
2021-11-19 18:25:25 +0700

    Added shell for lesson 1 and 2.1

---
+++ R-setup.R
@@ -0,0 +1,23 @@
+
+## Interactive training modules on National Forest Inventory data analysis
+## Gael Sola, FAO
+## October 2021
+
+## Setup package development for lessons
+## https://education.rstudio.com/blog/2020/09/delivering-learnr-tutorials-in-a-package/
+
+
+## Libs
+#install.packages(c("devtools", "roxygen2", "learnr"))
+library(devtools)
+library(usethis)
+library(roxygen2)
+library(learnr)
+
+
+## Make package
+usethis::create_package("D:/github-repos/eNFI")
+
+## Make lessons
+usethis::use_tutorial("eNFI-lesson1", "Overview of the preliminary data", open = interactive())
+usethis::use_tutorial("eNFI-lesson2.1", "Simple sampling for carbon - Design", open = interactive())
