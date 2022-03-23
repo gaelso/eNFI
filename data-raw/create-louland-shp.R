8327a5d3dd7a623316e4d4fab70459507769e11b
gael.sola <gaelsola@hotmail.fr>
2022-03-24 05:15:08 +0700

    adding maps

--- data-raw/create-louland-shp.R
+++ data-raw/create-louland-shp.R
@@ -10,8 +10,15 @@ library(tidyverse)
 ## Data preparation
 #louland_lc90m <- stars::read_stars(system.file("extdata/louland_lc90m.tif", package = "eNFI", mustWork = T))
 louland_lc90m <- stars::read_stars("inst/extdata/louland_lc90m.tif")
+load("data/louland_param.rda")
 
 plot(louland_lc90m)
+
+## Fonts for mapping
+sysfonts::font_add("Lora", "inst/fonts/lora-v23-latin-italic.ttf")
+showtext::showtext_auto()
+
+
 
 ## Create shapefiles
 t1 <- Sys.time()
@@ -51,4 +58,44 @@ louland_admin <- louland_lc %>%
 
 save(louland_lc, file = "inst/extdata/louland_lc.Rda", compress = "xz")
 save(louland_admin, file = "inst/extdata/louland_admin.Rda", compress = "xz")
+
+## Create maps
+sf_lc <- louland_lc %>%
+  left_join(tibble(lc_id = 0:6, lc_name = c("Water", "Non-forest", "Woodland", "Deciduous", "Mixed-deciduous", "Evergreen", "Mangrove")), by = "lc_id") %>%
+  mutate(lc_name = fct_reorder(lc_name, lc_id))
+sf_admin <- louland_admin
+pal <- louland_param$hex
+nb_ft <- 4
+
+gr_map <- ggplot() +
+  geom_sf(data = sf_lc, aes(fill = lc_name), col= NA) +
+  scale_fill_manual(values = pal) +
+  geom_sf(data = sf_admin, fill = NA, size = 1, color = "black") +
+  theme_bw() +
+  theme(
+    panel.background = element_rect(fill = "#73c2fb"),
+    text = element_text(family = "Lora", face = "italic", size = 16),
+  ) +
+  labs(fill = "") +
+  coord_sf(xlim = c(-20.5, -19.5), ylim = c(-0.8, 0.2), expand = FALSE, crs = st_crs(4326)) +
+  ggspatial::annotation_scale(
+    location = "tr",
+    bar_cols = c("grey60", "white"),
+    text_family = "Lora",
+    text_face = "italic"
+  ) +
+  ggspatial::annotation_north_arrow(
+    location = "tr", 
+    which_north = "true",
+    pad_x = unit(0.2, "in"), 
+    pad_y = unit(0.3, "in"),
+    style = ggspatial::north_arrow_nautical(
+      fill = c("grey40", "white"),
+      line_col = "grey20",
+      text_family = "Lora",
+      text_face = "italic"
+    )
+  )
+gr_map  
+
 
